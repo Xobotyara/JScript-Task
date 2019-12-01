0b627762c7c9fa5e43f0dd5403366dd7ad09a24a
Xobotyara <probosciss@rambler.ru>
2019-12-01 14:43:18 +0600

    leftside

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     let leftSide = document.getElementById('left-side');
     let rightSide = document.getElementById('right-side');
+    let leftContainer = document.querySelector('.left-side__container');
     let leftItemInfo = document.getElementById('left__item-info');
+    let leftSelector = document.querySelector('.left-side__selector');
     let leftImg = document.getElementById('left__item-img');
     let leftCalculator = document.querySelector('.left-side__item-calculator');
     let leftCostBox = document.querySelector('.item-calculator__cost');
@@ -17,25 +19,63 @@ document.addEventListener('DOMContentLoaded', function(){
             rightSide.classList.add('maximaized');
             leftSide.classList.add('minimaized');
 
+            leftContainer.classList.add('scrolled');
+            leftSelector.classList.add('hidden');
+
             leftBtn.classList.remove('go-right');
             rightBtn.classList.remove('go-right');
             leftBtn.classList.add('go-left');
             rightBtn.classList.add('go-left');
+
+            leftImg.classList.remove('animated');
+            leftCalculator.classList.remove('animated');
+            leftCostBox.classList.remove('animated');
+            leftAvatars.classList.remove('animated');
+            leftAvatars.classList.add('visibleHidden');
+
+
+
         } else if(leftSide.classList.contains('minimaized')){
 
             leftSide.classList.remove('minimaized');
             rightSide.classList.remove('maximaized');
 
+            leftContainer.classList.remove('scrolled');
+            leftSelector.classList.remove('hidden');
+
             leftBtn.classList.remove('go-left');
             rightBtn.classList.remove('go-left');
+
+            leftImg.classList.remove('animated');
+            leftCalculator.classList.remove('animated');
+            leftCostBox.classList.remove('animated');
+            leftAvatars.classList.remove('animated');
+            leftAvatars.classList.add('visibleHidden');
+
         } else{
 
             leftSide.classList.add('minimaized');
             rightSide.classList.add('maximaized');
 
+            leftContainer.classList.add('scrolled');
+            leftSelector.classList.add('hidden');
+
             leftBtn.classList.add('go-left');
             rightBtn.classList.add('go-left');
         }; 
+        
+        if (leftItemInfo.classList.contains('hidden')) {
+            leftItemInfo.classList.remove('hidden');
+            setTimeout(function () {
+                leftItemInfo.classList.remove('visibleHidden');
+            }, 20);
+        } else {
+            leftItemInfo.classList.add('visibleHidden');
+            setTimeout(function () {
+                leftItemInfo.classList.add('hidden');
+            }, 400);
+        };
+
     };
     leftBtn.onclick = function() {
         if(rightSide.classList.contains('minimaized')){
@@ -51,6 +91,9 @@ document.addEventListener('DOMContentLoaded', function(){
             rightSide.classList.add('minimaized');
             leftSide.classList.add('maximaized');
             leftSide.classList.remove('minimaized');
+
+            leftContainer.classList.remove('scrolled');
+            leftSelector.classList.remove('hidden');
 
             leftBtn.classList.remove('go-left');
             rightBtn.classList.remove('go-left');
@@ -99,10 +142,7 @@ document.addEventListener('DOMContentLoaded', function(){
         document.querySelector('.checked').classList.remove('checked');     
         let inputChecked = document.querySelector('input[type="radio"]:checked');
         inputChecked.parentElement.classList.add('checked');   
-        
-        
     }
-      
 
 
 
